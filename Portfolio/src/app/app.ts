40a2900fb274ea094f57a29cec957b7cbded574c
nico-leitz <ndesk.ltrz@gmail.com>
2026-06-11 01:10:56 +0200

    feat(home): merged header and hero section in home component

--- Portfolio/src/app/app.ts
+++ Portfolio/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./shared/components/header/header";
 import { HeroSection } from "./shared/components/hero-section/hero-section";
+import { Home } from "./pages/home/home";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, HeroSection],
+  imports: [RouterOutlet, Header, HeroSection, Home],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- Portfolio/src/app/pages/home/home.ts
+++ Portfolio/src/app/pages/home/home.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeroSection } from '../../shared/components/hero-section/hero-section';
+import { Header } from '../../shared/components/header/header';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Header ,HeroSection],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
