3d6c15d61ee5b8dfa3e167262b34ef54eb574ad6
nico-leitz <ndesk.ltrz@gmail.com>
2026-06-14 11:00:19 +0200

    feat(about_me): add html structure and placeholder image as well as basic styling

--- Portfolio/src/app/pages/home/home.ts
+++ Portfolio/src/app/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroSection } from '../../shared/components/hero-section/hero-section';
 import { Header } from '../../shared/components/header/header';
+import { AboutMe } from "../../shared/components/about-me/about-me";
 
 @Component({
   selector: 'app-home',
-  imports: [Header ,HeroSection],
+  imports: [Header, HeroSection, AboutMe],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
