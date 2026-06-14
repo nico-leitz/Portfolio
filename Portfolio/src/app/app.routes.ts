a9d8cc7787f029169781281b5f0915f51c8140ba
nico-leitz <ndesk.ltrz@gmail.com>
2026-06-14 10:46:19 +0200

    feat: add routing for path and changed styling for hero component

--- Portfolio/src/app/app.routes.ts
+++ Portfolio/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { AboutMe } from './shared/components/about-me/about-me';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "home",
+        component: Home
+    },
+    {
+        path: "about_me",
+        component: AboutMe
+    }
+];
