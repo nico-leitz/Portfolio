77ea88ed62d31441ce7c37fbb1aadfeceb47d0c2
nico-leitz <ndesk.ltrz@gmail.com>
2026-06-14 12:48:47 +0200

    feat: add skills-section component to the project

---
+++ Portfolio/src/app/shared/components/skills-section/skills-section.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skills-section',
+  imports: [],
+  templateUrl: './skills-section.html',
+  styleUrl: './skills-section.scss',
+})
+export class SkillsSection {}
