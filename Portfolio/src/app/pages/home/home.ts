85c72d11c9663e5a7deedf7cb1555f8571eb09ab
nico-leitz <ndesk.ltrz@gmail.com>
2026-06-15 13:20:17 +0200

    refactor: change content limitation and styles of components to fit with 1440px

--- Portfolio/src/app/pages/home/home.ts
+++ Portfolio/src/app/pages/home/home.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { HeroSection } from '../../shared/components/hero-section/hero-section';
 import { Header } from '../../shared/components/header/header';
 import { AboutMe } from "../../shared/components/about-me/about-me";
+import { SkillsSection } from "../../shared/components/skills-section/skills-section";
+import { FeaturedProjects } from "../../shared/components/featured-projects/featured-projects";
 
 @Component({
   selector: 'app-home',
-  imports: [Header, HeroSection, AboutMe],
+  imports: [Header, HeroSection, AboutMe, SkillsSection, FeaturedProjects],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
