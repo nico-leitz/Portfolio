97cf716d9e01cdcbeab92ff61a99c1fdc3549fb3
nico-leitz <ndesk.ltrz@gmail.com>
2026-06-16 14:50:14 +0200

    feat: implent footer-component to the project

---
+++ Portfolio/src/app/shared/components/footer-section/footer-section.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-section',
+  imports: [],
+  templateUrl: './footer-section.html',
+  styleUrl: './footer-section.scss',
+})
+export class FooterSection {}
