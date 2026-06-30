7a8e20e0ae7b81370266b64cbbd9aa1b46f47133
nico-leitz <ndesk.ltrz@gmail.com>
2026-06-30 22:51:17 +0200

    refactor: main page overhaul (add links, adjust styling, made code a bit cleaner)

--- Portfolio/src/app/pages/imprint/imprint.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-imprint',
-  imports: [],
-  templateUrl: './imprint.html',
-  styleUrl: './imprint.scss',
-})
-export class Imprint {}
